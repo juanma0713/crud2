4d4328d980f33d01702407f3b9d1aff98b334db0
Juan_Ma <jmanuelrg.710@gmail.com>
2022-02-09 21:03:00 -0500

    funcionando

--- views/menu.php
+++ views/menu.php
@@ -1,12 +1,12 @@
 <?php
 
-define('APP_URL', 'http://localhost/CRUD/')
+define('APP_URL', 'http://localhost/crud2/')
 
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CRUD</a>
+    <a class="navbar-brand" href="#">crud2</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
